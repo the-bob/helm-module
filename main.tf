47850a5d4d5201b9186ea0e29f12f4f96273d0a7
the-bob <adelbertureta@MACBOOKPRO.station>
2022-05-04 19:58:31 +0100

    feat: init

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "helm" {
+  source  = "terraform-module/release/helm"
+  version = "2.7.0"
+
+  namespace  = var.namespace
+  repository = var.repository
+
+  values = var.values_file != null && var.values_file != "" ? [templatefile(var.values_file, var.values)] : []
+  app = {
+    name          = var.name
+    version       = var.chart_version
+    chart         = var.chart_name
+    force_update  = var.force_update
+    wait          = var.wait
+    recreate_pods = var.recreate_pods
+    force_update  = var.force_update
+    deploy        = 1
+  }
+
+  set           = var.set
+  set_sensitive = var.set_sensitive
+}
---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "name" {
+  description = "Name of the Chart"
+  type        = string
+}
+
+variable "repository" {
+  description = "Helm Repository"
+  type        = string
+}
+
+variable "values_file" {
+  description = "Helm Values File"
+  type        = string
+  default     = null
+}
+
+variable "values" {
+  description = "Override Values that will be added in the values file if the values_file is specified"
+  type        = map(string)
+  default     = {}
+}
+
+variable "chart_version" {
+  description = "Version of the chart to deploy"
+  type        = string
+}
+
+variable "chart_name" {
+  description = "The Chart from the chart museum to be deployed"
+  type        = string
+}
+
+variable "force_update" {
+  description = "Flag if the chart should be force update"
+  type        = bool
+  default     = true
+}
+
+variable "wait" {
+  description = "Flag if to wait until the deployment finishes"
+  type        = bool
+  default     = true
+}
+
+variable "recreate_pods" {
+  description = "Should it recreate the pods"
+  type        = bool
+  default     = true
+}
+
+variable "set" {
+  description = "Helm --set"
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = null
+}
+
+variable "set_sensitive" {
+  description = "--set that won't be shown in the terraform plan"
+  type = list(object({
+    path  = string
+    value = string
+  }))
+  default = null
+}
